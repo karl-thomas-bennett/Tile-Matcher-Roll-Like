140c4773e90bb87e7d9e44d8f9d040c301acf8db
Karl Bennett <karlthomasbennett@gmail.com>
2022-02-18 17:22:54 +1300

    Add random coloured tiles rendering

--- Assets/Scripts/GameGrid.cs
+++ Assets/Scripts/GameGrid.cs
@@ -6,10 +6,36 @@ public class GameGrid : MonoBehaviour
 {
     public int size;
     public float tileSize;
+    public List<GameObject> tilePool;
+    private GridData data;
     // Start is called before the first frame update
     void Start()
     {
-        
+        data = new GridData(size, tilePool.Count, transform);
+        UpdateGrid();
+    }
+
+    public void UpdateGrid()
+    {
+        //Create Tiles based on GridData
+        for(int i = 0; i < data.rows.Count; i++)
+        {
+            for(int j = 0; j < data.rows[i].Count; j++)
+            {
+                Instantiate(tilePool[data.rows[i][j]], new Vector3(-5 + j, i), Quaternion.identity, transform);
+            }
+        }
+        for (int i = 0; i < data.columns.Count; i++)
+        {
+            for (int j = 0; j < data.columns[i].Count; j++)
+            {
+                if(j < 5 || j >= 5 + size)
+                {
+                    Instantiate(tilePool[data.columns[i][j]], new Vector3(i, j-5), Quaternion.identity, transform);
+                }
+            }
+        }
+
     }
 
     // Update is called once per frame
--- Assets/Scripts/GridData.cs
+++ Assets/Scripts/GridData.cs
@@ -4,29 +4,45 @@ using UnityEngine;
 
 public class GridData
 {
-    private List<List<GameObject>> columns;
-    private List<List<GameObject>> rows;
+    public List<List<int>> columns;
+    public List<List<int>> rows;
+    private int poolSize;
     private List<int> visibleColumnStarts;
     private List<int> visibleRowStarts;
     private int size = -1;
     private int buffer;
+    private Transform parent;
 
-    public GridData(int size, int buffer = 5)
+    public GridData(int size, int poolSize, Transform parent, int buffer = 5)
     {
+        visibleColumnStarts = new List<int>();
+        visibleRowStarts = new List<int>();
+        columns = new List<List<int>>();
+        rows = new List<List<int>>();
+        this.poolSize = poolSize;
         this.buffer = buffer;
         SetGridSize(size);
         GenerateGrid();
     }
 
-    
 
     public void MoveColumn(int columnNumber, int amount)
     {
     }
 
-    public GameObject GenerateTile()
+    public int GenerateTile()
     {
-        return null;
+        return RandomInt(0, poolSize);
+    }
+
+    private int RandomInt(int min, int max)
+    {
+        int output = Random.Range(min, max);
+        while(output == max)
+        {
+            output = Random.Range(min, max);
+        }
+        return output;
     }
 
     public void ResolveMatches()
@@ -52,8 +68,8 @@ public class GridData
     {
         for (int i = 0; i < size; i++)
         {
-            rows.Add(new List<GameObject>());
-            columns.Add(new List<GameObject>());
+            rows.Add(new List<int>());
+            columns.Add(new List<int>());
             visibleRowStarts.Add(buffer);
             visibleColumnStarts.Add(buffer);
         }
@@ -62,7 +78,7 @@ public class GridData
             //Adding tiles to all rows
             for (int j = 0; j < size + 10; j++)
             {
-                GameObject tile = GenerateTile();
+                int tile = GenerateTile();
                 rows[i].Add(tile);
                 //Adding the same tile to columns when there is overlap
                 if (j >= buffer && j < buffer + size)
@@ -74,7 +90,7 @@ public class GridData
             //Adding all tiles to columns that do not overlap with rows
             for (int j = 0; j < size + 10; j++)
             {
-                GameObject tile = GenerateTile();
+                int tile = GenerateTile();
                 if(j < buffer)
                 {
                     columns[i].Insert(0, tile);
